b0c2f72404b2fa0f500c46ca28825b51ac0df5c9
LuIsMoReNo7 <1483853@senati.pe>
2023-03-17 17:52:08 -0500

    Perdon profe por faltar xd, aqui mi trabajo de hoy

---
+++ Positivo y negativo con condicionales/src/Algoritmo_positivo_negativo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Algoritmo_positivo_negativo {
+    public static void main(String[] args) {
+        Scanner dato = new Scanner(System.in);
+        String nombre = "";
+        System.out.println("Ingrese su nombre: ");
+        nombre = dato.nextLine();
+        System.out.println("hola! " + nombre);
+        System.out.println("Ingrese el numero");
+        int num = dato.nextInt();
+        if (num > 0){
+            System.out.println("hola : " + nombre + " " + "su numero" + num + " Es positivo! ");
+        }
+        else if (num == 0) {
+            System.out.println("hola : " + nombre + " " + "Su numero 0 no es negativo y tampoco es positivo! ");
+        }
+        else{
+            System.out.println("Hola : " + nombre + " " +  "Su numero " + num + " es negativo! ");
+        }
+    }
+}
+    
